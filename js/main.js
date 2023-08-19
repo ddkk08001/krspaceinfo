0e66a863525dbe214d0b670e086d004d9af72742
ddkk08 <daniel.sh08.kim@gmail.com>
2023-08-19 19:11:42 +0200

    팝업창 미리 정리

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+미래 팝업을 위한 코드
+
+function openPop() {
+    document.getElementById("popup_layer").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop() {
+    document.getElementById("popup_layer").style.display = "none";
+}
+
+*/
