35d047733137583ca62d1619bd43a6000b9ccd05
ddkk08 <daniel.sh08.kim@gmail.com>
2023-08-20 14:58:15 +0200

    이벤트 칸 개발 (배경 제외)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const dayEl = document.getElementById('day')
+const hourEl = document.getElementById('hour')
+const minEl = document.getElementById('min')
+const secondEl = document.getElementById('sec')
 /*
 미래 팝업을 위한 코드
 
@@ -12,3 +16,27 @@ function closePop() {
 }
 
 */
+
+//타이머
+function countDown(){
+    const newYear = new Date(2022, 8, 5)
+    const current = new Date()
+
+    const diffTime = Math.abs(newYear-current)
+
+    const day = Math.floor( diffTime/(1000*3600*24) )
+    const hour = Math.floor( diffTime/(1000*3600) %24)
+    const min = Math.floor( diffTime/(1000*60) %60)
+    const second = Math.floor( diffTime/(1000) %60)
+
+    dayEl.innerHTML = formatTime(day)
+    hourEl.innerHTML = formatTime(hour)
+    minEl.innerHTML = formatTime(min)
+    secondEl.innerHTML = formatTime(second)
+}
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : (time);
+}
+countDown()
+
+setInterval(countDown, 1000)
